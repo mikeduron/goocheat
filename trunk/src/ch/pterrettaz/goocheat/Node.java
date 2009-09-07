f4c0617794f12d3afdcb729adf09c3e42091173b
pierrick <pierrick@85ba5538-9b8e-11de-993b-6301ed8e1f60>
2009-09-07 09:24:23 +0000

    adding files

---
+++ trunk/src/ch/pterrettaz/goocheat/Node.java
@@ -0,0 +1,73 @@
+package ch.pterrettaz.goocheat;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Node {
+
+    private final Map<Integer, Node> children;
+    private final Node parent;
+    private final int key;
+    
+    public Node() {
+    	this(null, -1);
+    }
+    
+    public Node(Node parent, int key) {
+		this.parent = parent;
+		this.children = new LinkedHashMap<Integer, Node>();
+		this.key = key;
+    }
+    
+    public boolean isLeaf() {
+    	return children.size() == 0;
+    }
+    
+    public void add(String token) {
+    	if (token.length() > 0) {
+    		int key;
+    		do {
+    			key = token.charAt(0);
+    			token = token.substring(1);
+    		} while (token.length() > 1 && key == ' ');
+
+    		Node child = children.get(key);
+    		if (child == null) {
+    			child = new Node(this, key);
+    		}
+    		child.add(token);
+    		children.put(key, child);
+    	}
+    }
+    
+    public Node getParent() {
+    	return parent;
+    }
+    
+    public Collection<Node> getChildren() {
+    	return children.values();
+    }
+    
+    public Node getChild(int key) {
+    	return children.get(key);
+    }
+    
+    public int getKey() {
+    	return key;
+    }
+
+    public boolean isRoot() {
+    	return getKey() == -1;
+    }
+    
+    public boolean hasPrefix(String s) {
+    	if (s.length() > 0) {
+			Node child = getChild(s.charAt(0));
+			if (child != null)
+				return child.hasPrefix(s.substring(1));
+			return false;
+		}
+    	return true;
+    }
+}
