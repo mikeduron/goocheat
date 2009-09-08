9707dc9cff8045ccb6d7d95bcc1b4c6e38612b6f
pierrick <pierrick@85ba5538-9b8e-11de-993b-6301ed8e1f60>
2009-09-08 21:51:16 +0000

    code format

--- src/ch/pterrettaz/goocheat/Main.java
+++ src/ch/pterrettaz/goocheat/Main.java
@@ -21,106 +21,110 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-public class Main extends JFrame{
-	private static final long serialVersionUID = 1L;
-	protected final GooCheat gooCheat;
-	private final JTextField text;
-	private final JButton button;
-	private final JTextArea result;
-	
-	public Main(final GooCheat gooCheat) {
-		this.gooCheat = gooCheat;
-		String version = "";
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("version.txt")));
-			version = in.readLine();
-			in.close();
-		} catch (Exception e1) { }
-		setTitle("Goo Cheat - " + version);
-		setSize(300, 200);
-		Dimension windowDim = Toolkit.getDefaultToolkit().getScreenSize();
-		setLocation(windowDim.width/2 - getWidth()/2, windowDim.height/2 - getHeight()/2);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.insets = new Insets(5, 5, 5, 5);
-		c.weightx = 1;
-		c.weighty = 0;
-		c.gridx = 0;
-		c.gridy = 0;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		text = new JTextField();
-		text.addKeyListener(new KeyListener() {
-			public void keyTyped(KeyEvent e) {
-				JTextField source = (JTextField) e.getSource();
-				if (source.getText().length() > 19)
-					source.setText(source.getText().substring(0, 19));	
-			}
-			public void keyReleased(KeyEvent e) {
-				
-			}
-			public void keyPressed(KeyEvent e) {
-				JTextField source = (JTextField) e.getSource();
-				if (source.getText().length() < 15) {
-					button.setEnabled(false);
-					performSearch();
-				} else {
-					button.setEnabled(true);
-				}
-			}
-		});
-		
-		panel.add(text, c);
-		
-		c.weightx = 0;
-		c.gridx = 1;
-		c.fill = GridBagConstraints.NONE;
-		button = new JButton("generate");
-		button.setEnabled(false);
-		panel.add(button, c);
-		
-		c.gridx = 0;
-		c.gridy = 2;
-		c.weightx = 1;
-		c.weighty = 1;
-		c.gridwidth = 2;
-		c.fill = GridBagConstraints.BOTH;
-		result = new JTextArea();
-		panel.add(result, c);
-		setContentPane(panel);
-		setVisible(true);
-		getRootPane().setDefaultButton(button);
-		
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				button.setEnabled(false);
-				performSearch();
-				button.setEnabled(true);
-			}
-		});
-	}
-	
-	private void performSearch() {
-		try {
-			result.setText("");
-			if (text.getText().equals(""))
-				return;
-			
-			final List<String> ret = new LinkedList<String>();
-			SwingUtilities.invokeLater(new Runnable() {
-				public void run() {
-					ret.addAll(gooCheat.getValidWordsPermutation(text.getText()));
-					for (String word: ret) {
-						result.append(word + '\n');
-					}
-				}
-			});
-		} catch (Exception ex) { }
-	}
-	
-	
-	public static void main(String[] args) throws Exception {
-		new Main(new GooCheat(10));
-	}
+public class Main extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+    protected final GooCheat gooCheat;
+    private final JTextField text;
+    private final JButton button;
+    private final JTextArea result;
+
+    public Main(final GooCheat gooCheat) {
+        this.gooCheat = gooCheat;
+        String version = "";
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("version.txt")));
+            version = in.readLine();
+            in.close();
+        } catch (Exception e1) {
+        }
+        setTitle("Goo Cheat - " + version);
+        setSize(300, 200);
+        Dimension windowDim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(windowDim.width / 2 - getWidth() / 2, windowDim.height / 2 - getHeight() / 2);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(5, 5, 5, 5);
+        c.weightx = 1;
+        c.weighty = 0;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        text = new JTextField();
+        text.addKeyListener(new KeyListener() {
+            public void keyTyped(KeyEvent e) {
+                JTextField source = (JTextField) e.getSource();
+                if (source.getText().length() > 19)
+                    source.setText(source.getText().substring(0, 19));
+            }
+
+            public void keyReleased(KeyEvent e) {
+
+            }
+
+            public void keyPressed(KeyEvent e) {
+                JTextField source = (JTextField) e.getSource();
+                if (source.getText().length() < 15) {
+                    button.setEnabled(false);
+                    performSearch();
+                } else {
+                    button.setEnabled(true);
+                }
+            }
+        });
+
+        panel.add(text, c);
+
+        c.weightx = 0;
+        c.gridx = 1;
+        c.fill = GridBagConstraints.NONE;
+        button = new JButton("generate");
+        button.setEnabled(false);
+        panel.add(button, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        c.weightx = 1;
+        c.weighty = 1;
+        c.gridwidth = 2;
+        c.fill = GridBagConstraints.BOTH;
+        result = new JTextArea();
+        panel.add(result, c);
+        setContentPane(panel);
+        setVisible(true);
+        getRootPane().setDefaultButton(button);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                button.setEnabled(false);
+                performSearch();
+                button.setEnabled(true);
+            }
+        });
+    }
+
+    private void performSearch() {
+        try {
+            result.setText("");
+            if (text.getText().equals(""))
+                return;
+
+            final List<String> ret = new LinkedList<String>();
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    ret.addAll(gooCheat.getValidWordsPermutation(text.getText()));
+                    for (String word : ret) {
+                        result.append(word + '\n');
+                    }
+                }
+            });
+        } catch (Exception ex) {
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Main(new GooCheat(10));
+    }
 }
--- src/ch/pterrettaz/goocheat/Node.java
+++ src/ch/pterrettaz/goocheat/Node.java
@@ -10,85 +10,85 @@ public class Node {
     private final Node parent;
     private final int key;
     private boolean hasData = false;
-    
+
     public Node() {
-    	this(null, -1);
+        this(null, -1);
     }
-    
+
     public Node(Node parent, int key) {
-		this.parent = parent;
-		this.children = new LinkedHashMap<Integer, Node>();
-		this.key = key;
+        this.parent = parent;
+        this.children = new LinkedHashMap<Integer, Node>();
+        this.key = key;
     }
-    
+
     public boolean isLeaf() {
-    	return children.size() == 0;
+        return children.size() == 0;
     }
-    
+
     public void add(String token) {
-    	if (token.length() == 0) {
-    	    hasData = true;
-    	} else {
-    	    int key;
-    	    do {
-    		key = token.charAt(0);
-    		token = token.substring(1);
-    	    } while (token.length() > 1 && key == ' ');
-    	    
-    	    Node child = children.get(key);
-    	    if (child == null) {
-    		child = new Node(this, key);
-    	    }
-    	    child.add(token);
-    	    children.put(key, child);
-    	}
+        if (token.length() == 0) {
+            hasData = true;
+        } else {
+            int key;
+            do {
+                key = token.charAt(0);
+                token = token.substring(1);
+            } while (token.length() > 1 && key == ' ');
+
+            Node child = children.get(key);
+            if (child == null) {
+                child = new Node(this, key);
+            }
+            child.add(token);
+            children.put(key, child);
         }
-    
+    }
+
     public Node getParent() {
-    	return parent;
+        return parent;
     }
-    
+
     public Collection<Node> getChildren() {
-    	return children.values();
+        return children.values();
     }
-    
+
     public Node getChild(int key) {
-    	return children.get(key);
+        return children.get(key);
     }
-    
+
     public int getKey() {
-    	return key;
+        return key;
     }
 
     public boolean isRoot() {
-    	return getKey() == -1;
-    }
-    
-    public boolean hasPrefix(String s) {
-    	if (s.length() > 0) {
-			Node child = getChild(s.charAt(0));
-			if (child != null)
-				return child.hasPrefix(s.substring(1));
-			return false;
-		}
-    	return true;
-    }
-    
-    @Override
-    public String toString() {
-    	char k = (char)key;
-    	return k + "";
+        return getKey() == -1;
     }
 
-	public boolean exists(String s) {
-		if (s.length() > 0) {
-			Node child = getChild(s.charAt(0));
-			if (child == null) {
-				return false;
-			} else {
-				return child.exists(s.substring(1));
-			}
-		}
-		return hasData;
-	}
+    public boolean hasPrefix(String s) {
+        if (s.length() > 0) {
+            Node child = getChild(s.charAt(0));
+            if (child != null)
+                return child.hasPrefix(s.substring(1));
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        char k = (char) key;
+        return k + "";
+    }
+
+    public boolean exists(String s) {
+        if (s.length() > 0) {
+            Node child = getChild(s.charAt(0));
+            if (child == null) {
+                return false;
+            } else {
+                return child.exists(s.substring(1));
+            }
+        }
+        return hasData;
+    }
 }
